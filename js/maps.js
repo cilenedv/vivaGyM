264953fcd32862d369c00e8b7f24cd172a80014b
yeiny gonzalez cifuentes <yeinygonzalezcifuentes@gmail.com>
2016-12-05 16:56:38 -0500

    insertando js de busqueda

--- js/maps.js
+++ js/maps.js
@@ -32,6 +32,37 @@ var error = function (error) {
     console.log(error);
 };
 
-
+$('.search1').on('click', function() {
+    // Obtenemos la dirección y la asignamos a una variable
+    var address = $('.search').val();
+    // Creamos el Objeto Geocoder
+    var geocoder = new google.maps.Geocoder();
+    // Hacemos la petición indicando la dirección e invocamos la función
+    // geocodeResult enviando todo el resultado obtenido
+    geocoder.geocode({ 'address': address}, geocodeResult);
+});
+ 
+var geocodeResult= function(results, status) {
+    // Verificamos el estatus
+    if (status == 'OK') {
+        // Si hay resultados encontrados, centramos y repintamos el mapa
+        // esto para eliminar cualquier pin antes puesto
+        var mapOptions = {
+            center: results[0].geometry.location,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map($(".mapa").get(0), mapOptions);
+        // fitBounds acercará el mapa con el zoom adecuado de acuerdo a lo buscado
+        map.fitBounds(results[0].geometry.viewport);
+        // Dibujamos un marcador con la ubicación del primer resultado obtenido
+        var markerOptions = { position: results[0].geometry.location }
+        var marker = new google.maps.Marker(markerOptions);
+        marker.setMap(map);
+    } else {
+        // En caso de no haber resultados o que haya ocurrido un error
+        // lanzamos un mensaje con el error
+        alert("Geocoding no tuvo éxito debido a: " + status);
+    }
+}
 
 $(document).ready(load);
