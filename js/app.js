fc9ec78772f57a28f66da62d0a7aa9f7bdc641a5
zafiroazul <mchristy.ccb@gmail.com>
2016-12-06 08:04:53 -0500

    añadir editado

--- js/app.js
+++ js/app.js
@@ -2,40 +2,10 @@
 
 $('.dropdown-toggle').dropdown();
 
-// $("#editar-lista-entidades").click(function(){
-// 	$(".edit-hidden").toggle();
-// });
-
-// $(".edit-hidden").click(function(){
-// 	// var npList = $(".np-list").val();
-//     var txt2 = $("<input></input>").attr("id","textBox").attr("type","text").attr("val"," ");
-//     $(".campo-edit").empty();
-//     $(".campo-edit").append(txt2);
-// })
-
-// $("#textBox").keyup(function(e) {
-
-//     if (e.keycode == 13) {
-//         // $('<div><span>' + $('#textBox').val() + '</span>&nbsp;<a href="javascript:;" onclick="$(this).parent().remove();">X</a>').appendTo();
-//         $("#textBox").val("");
-//     };
-// })
-
 ;(function(){
 	window.addEventListener("load", listaEditarEntidad);
 		var edListEnt = document.getElementById("editar-lista-entidades");
 		var btnEditNombre = document.querySelectorAll(".lap-editor");
-		// var entradaDos = document.getElementById("dos");
-		// var entradaTres = document.getElementById("tres");
-
-		// var container = document.getElementById("container");
-
-		// var contenedor = document.getElementById("contenedor");
-		// var contenedorTodo = document.getElementById("form");
-		// var tituloAndLink = document.getElementById("input");
-		// var textAreaDiario = document.getElementById("textarea");
-		// var botonDiario = document.getElementById("button");
-		// var botonRetro = document.getElementById("buttonRetro");
 
 		function listaEditarEntidad(){
 			edListEnt.addEventListener("click", mostrarBtnEdit);
@@ -47,65 +17,52 @@ $('.dropdown-toggle').dropdown();
     		for (i = 0; i < btnEditNombre.length; i++) {
      		   	btnEditNombre[i].classList.remove("edit-hidden");
     		}
-			// btnEditNombre.addEventListener("click",campoEdit);
 		}
 
-		$(".lap-editor").click(function(){
-			var contenEdit = $(this).prev();
-			$(contenEdit).attr("contenteditable","true");
-			// $(contenEdit).focus();
-		})
-
-		// function imprimirMeme(e){
-		// 	e.preventDefault();
-		// 	var imagenMeme = document.createElement("img");
-		// 	imagenMeme.setAttribute("src", tituloAndLink.value);
-		// 	vacios();
-		// 	contenedor.insertBefore(imagenMeme, contenedor.childNodes[0]).classList.add("col-xs-12","col-sm-12","col-md-12");
-		// 	botonRetro.classList.remove("ocultarElem");
-		// 	contenedorTodo.classList.add("ocultarElem");
-		// 	botonRetro.addEventListener("click", volver);
-		// }
-
-		// function ocultarCarruselDiario(e){
-		// 	e.preventDefault();
-		// 	carrusel.classList.add("ocultarElem");
-		// 	contenedorTodo.classList.remove("ocultarElem");
-		// 	botonDiario.addEventListener("click", imprimirDiario);			
-		// }
-
-		// function imprimirDiario(e){
-		// 	e.preventDefault();
-		// 	var titleDiario = document.createElement("div");
-		// 	var cajaDiario = document.createElement("div");
-		// 	cajaDiario.insertBefore(titleDiario, cajaDiario.childNodes[0]).innerText = tituloAndLink.value;
-		// 	cajaDiario.appendChild(document.createTextNode(textAreaDiario.value));
-		// 	vacios();
-		// 	contenedor.insertBefore(cajaDiario, contenedor.childNodes[0]);
-		// 	contenedorTodo.classList.add("ocultarElem");
-		// 	botonRetro.classList.remove("ocultarElem");
-		// 	botonRetro.addEventListener("click", volver);
-		// }
-
-		// function volver(){
-		// 	carrusel.classList.remove("ocultarElem");
-		// 	this.parentElement.firstChild.classList.add("ocultarElem");
-		// 	this.classList.add("ocultarElem");
-		// }
-		// function contenidos(){
-		// 	sectionContenedor.insertBefore(contenedor, sectionContenedor.childNodes[0]);
-		// 	contenedor.insertBefore(tituloAndLink, contenedor.childNodes[0]);
-		// 	contenedor.insertBefore(textAreaDiario, contenedor.childNodes[1]);
-		// 	contenedor.appendChild(botonDiario).innerText = "Guardar";
-		// 	tituloAndLink.classList.add("form-control");
-		// 	tituloAndLink.setAttribute("autofocus", "focus");
-
-		// 	botonDiario.classList.add("btn", "btn-secundary");
-		// 	textAreaDiario.classList.add("form-control");
-		// 	textAreaDiario.setAttribute("rows", "14");
-		// }
-
-
-
 })();
+
+var editarLista = function(){
+	$(".lap-editor").click(lapizEditor);
+	$(".np-list").keypress(guardadoEditado);
+}
+
+$(document).ready(editarLista);
+
+var lapizEditor = function(){
+	var contenEdit = $(this).prev();
+		$(contenEdit).attr("contenteditable","true");
+		$(contenEdit).focus();
+		$(contenEdit).parent().addClass("inputCajaEdit");
+}
+
+var guardadoEditado = function(e){
+	if (e.keycode == 13) {
+		$(".lap-editor").remove();
+	}
+	return e.which != 13;
+	$(this).parent().removeClass("inputCajaEdit");
+}
+// $(".lap-editor").ready(function(){
+// 	$(".lap-editor").click(function(){
+// 		var contenEdit = $(this).prev();
+// 		$(contenEdit).attr("contenteditable","true");
+// 		$(contenEdit).focus();
+// 		$(contenEdit).parent().addClass("inputCajaEdit");
+// 		$(contenEdit).keypress(clickEditado);
+// 	});
+// 			// $(contenEdit).keypress(function(e){
+// 			// 	if (e.keycode == 13) {
+// 			// 		$(this).removeAtrr("contenteditable");
+// 			// 		$(this).parent().removeClass("inputCajaEdit");
+// 			// 	}
+// 			// })
+// })
+// 		var clickEditado = function(e){
+// 			if (e.keycode == 13) {
+// 				$(this).removeAtrr("contenteditable");
+// 				$(this).parent().removeClass("inputCajaEdit");
+// 			}
+// 		}
+
+
 
