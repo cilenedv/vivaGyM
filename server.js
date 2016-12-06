4b3b3f8bf25eb5957bd88c1327c1d9e81bc8683a
janetquispeu@gmail.com <janetquispeu@gmail.com>
2016-12-06 15:08:51 -0600

    proyecto final

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var express = require("express");
-var app = express();
-app.use(express.static(__dirname+ "/public"));
-app.listen(3000, function(){
-	console.log("Servidor encendido");
+var express=require("express");
+var app=express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.set("port",process.env.PORT || 3000);//hvhyu
+
+app.listen(app.get("port"),function() {
+	console.log("servidor encendido");
 });
