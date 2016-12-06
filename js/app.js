0e2f2e9d59cf8f84a958c6dd9d474a3ba884a84d
zafiroazul <mchristy.ccb@gmail.com>
2016-12-06 09:43:30 -0500

    añadiendo arreglo de botones

--- js/app.js
+++ js/app.js
@@ -2,30 +2,21 @@
 
 $('.dropdown-toggle').dropdown();
 
-;(function(){
-	window.addEventListener("load", listaEditarEntidad);
-		var edListEnt = document.getElementById("editar-lista-entidades");
-		var btnEditNombre = document.querySelectorAll(".lap-editor");
-
-		function listaEditarEntidad(){
-			edListEnt.addEventListener("click", mostrarBtnEdit);
-		}
-
-		function mostrarBtnEdit(){
-			// btnEditNombre.classList.remove("edit-hidden");
-			var i;
-    		for (i = 0; i < btnEditNombre.length; i++) {
-     		   	btnEditNombre[i].classList.remove("edit-hidden");
-    		}
-		}
-
-})();
-
 var editarLista = function(){
+	$("#editar-lista-entidades").click(botonLapiz);
 	$(".lap-editor").click(lapizEditor);
+	$(".np-list").keypress(guardadoEditado);
 }
 
 $(document).ready(editarLista);
+
+var botonLapiz = function(){
+	var btnEditNombre = $(".lap-editor").toggle();
+	var i;
+    for (i = 0; i < $(btnEditNombre).length; i++) {
+      	$(btnEditNombre)[i].classList.remove("edit-hidden");
+   	}
+}
 
 var lapizEditor = function(){
 	var contenEdit = $(this).prev();
@@ -33,34 +24,16 @@ var lapizEditor = function(){
 		$(contenEdit).focus();
 		$(contenEdit).parent().addClass("inputCajaEdit");
 }
-	$(".np-list").keypress(function(e){
+
+var guardadoEditado = function(e){
+	if (e.which != 13) {
+		return true;
+	}
+	else{
 		$(this).attr("contenteditable","false");
-		$(this).parent().removeClass("inputCajaEdit");
-		$(this).next().remove();	
-		return e.which != 13;
-	});
-	// 
-// $(".lap-editor").ready(function(){
-// 	$(".lap-editor").click(function(){
-// 		var contenEdit = $(this).prev();
-// 		$(contenEdit).attr("contenteditable","true");
-// 		$(contenEdit).focus();
-// 		$(contenEdit).parent().addClass("inputCajaEdit");
-// 		$(contenEdit).keypress(clickEditado);
-// 	});
-// 			// $(contenEdit).keypress(function(e){
-// 			// 	if (e.keycode == 13) {
-// 			// 		$(this).removeAtrr("contenteditable");
-// 			// 		$(this).parent().removeClass("inputCajaEdit");
-// 			// 	}
-// 			// })
-// })
-// 		var clickEditado = function(e){
-// 			if (e.keycode == 13) {
-// 				$(this).removeAtrr("contenteditable");
-// 				$(this).parent().removeClass("inputCajaEdit");
-// 			}
-// 		}
+		$(this).parent().removeClass("inputCajaEdit");	
+	}
+}
 
 
 
