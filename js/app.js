f5ade4fb88400e6ec9118cff8213cdccf3487319
zafiroazul <mchristy.ccb@gmail.com>
2016-12-06 10:42:26 -0500

    añadiendo fecha

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var editarLista = function(){
 	$("#editar-lista-entidades").click(botonLapiz);
 	$(".lap-editor").click(lapizEditor);
 	$(".np-list").keypress(guardadoEditado);
+	$(".np-list").keypress(horaEditar);
+	$(".np-list").keypress(fechaEditar);	
 }
 
 $(document).ready(editarLista);
@@ -35,5 +37,33 @@ var guardadoEditado = function(e){
 	}
 }
 
+var horaEditar = function(){
+	var fecha = new Date().toLocaleTimeString();
+	// var hora = fecha.getHours();
+	// var minuto = fecha.getMinutes();
+	// if (minuto < 10) {
+	// minuto = "0" + minuto;
+	// 		}
+	// var horaImprimible = hora + " : " + minuto;
+	$("#horaEditada").text(fecha);
+    // contenedor.insertBefore(horario, contenedor.childNodes[0]).classList.add("horaBox");
+}
 
+var fechaEditar = function(){
+	// $.date = function(dateObject) {
+    	var d = new Date();
+    	var day = d.getDate();
+    	var month = d.getMonth() + 1;
+    	var year = d.getFullYear();
+    	if (day < 10) {
+        	day = "0" + day;
+    	}
+    	if (month < 10) {
+        	month = "0" + month;
+    	}
+    	var date = day + "/" + month + "/" + year;
+		// return date;
+	// }
+	$("#fechaEditar").text(date);
+}
 
